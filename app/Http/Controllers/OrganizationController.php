591276e7c8ad54c93d1c96c1e0fb0ccec0c9f917
Jigos <nikitzhig6789@gmail.com>
2025-02-05 21:32:50 +0800

    Controller + fix

---
+++ app/Http/Controllers/OrganizationController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+use App\Models\Organization;
+use App\Models\Building;
+
+class OrganizationController extends Controller
+{
+    public function getOrganizationsByBuilding($buildingId)
+    {
+        $organizations = Organization::where('building_id', $buildingId)->get();
+        return response()->json(['organizations' => $organizations]);
+    }
+
+    public function getOrganizationsByActivity($activityId)
+    {
+        $organizations = Organization::whereHas('activities', function ($query) use ($activityId) {
+            $query->where('activity_id', $activityId);
+        })->get();
+
+        return response()->json(['organizations' => $organizations]);
+    }
+
+    public function getOrganizationsInRadius(Request $request)
+    {
+        $data = $request->validate([
+            'radius' => 'required',
+            'latitude' => 'required',
+            'longitude' => 'required',
+        ]);
+
+        $organizations = Organization::selectRaw("*, (6371 * acos(cos(radians(?)) * cos(radians(latitude)) * cos(radians(longitude) - radians(?)) + sin(radians(?)) * sin(radians(latitude)))) AS distance", [$data->latitude, $data->longitude, $data->latitude])
+            ->having("distance", "<", $data->radius)
+            ->get();
+
+        return response()->json(['organizations' => $organizations]);
+    }
+
+    public function getOrganizationById($organizationId)
+    {
+        $organization = Organization::find($organizationId);
+        if ($organization) {
+            return response()->json(['organization' => $organization]);
+        }
+        return response()->json(['error' => 'Organization not found'], 404);
+    }
+
+    public function searchOrganizationsByActivity($activityId)
+    {
+        $activityIds = Activity::where('parent_id', $activityId)->pluck('id')->toArray();
+        $activityIds[] = $activityId;
+
+        $organizations = Organization::whereHas('activities', function ($query) use ($activityIds) {
+            $query->whereIn('activity_id', $activityIds);
+        })->get();
+
+        return response()->json(['organizations' => $organizations]);
+    }
+
+    public function searchOrganizationsByName(Request $request)
+    {
+        $name = $request->validate([
+            'name' => 'required',
+        ]);
+        $organizations = Organization::where('name', 'LIKE', "%{$name}%")->get();
+        return response()->json(['organizations' => $organizations]);
+    }
+}
--- database/migrations/2025_02_05_124400_create_activity_organization_table.php
+++ database/migrations/2025_02_05_124400_create_activity_organization_table.php
@@ -15,7 +15,6 @@ return new class extends Migration
             $table->id();
             $table->foreignId('organization_id')->constrained()->onDelete('cascade');
             $table->foreignId('activity_id')->constrained()->onDelete('cascade');
-            $table->timestamps();
         });
     }
 
