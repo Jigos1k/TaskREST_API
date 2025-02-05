521a56ca14bbd93eb37843e745f31c15b3474f00
Jigos <nikitzhig6789@gmail.com>
2025-02-05 22:31:53 +0800

    fix

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\{OrganizationController, };
+use App\Http\Controllers\{OrganizationController, CreateController};
 
 Route::get('/organizations/building/{buildingId}', [OrganizationController::class, 'getOrganizationsByBuilding']);
 Route::get('/organizations/activity/{activityId}', [OrganizationController::class, 'getOrganizationsByActivity']);
